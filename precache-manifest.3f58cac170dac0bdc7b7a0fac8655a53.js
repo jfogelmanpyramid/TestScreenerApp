a8edb00fdf301fec5c525ec8b206273abb22181f
jfogelmanpyramid <jfogelman@pyramidsystems.com>
2020-09-17 18:20:59 -0400

    Updates

---
+++ precache-manifest.3f58cac170dac0bdc7b7a0fac8655a53.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a366425421e9ba5f4555a8b63edcd5a2",
+    "url": "/jfogelmanpyramid/TestScreenerApp/index.html"
+  },
+  {
+    "revision": "4eb659771b0d9bd859af",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/css/main.c784f7a1.chunk.css"
+  },
+  {
+    "revision": "388502395655db9fb9c4",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/js/2.fbbc5652.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/js/2.fbbc5652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb659771b0d9bd859af",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/js/main.017066b8.chunk.js"
+  },
+  {
+    "revision": "118e4a7de9551d483ac6",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/js/runtime-main.c4b11be9.js"
+  },
+  {
+    "revision": "6dc305f4dc6b230b726548d2d2848395",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/DarkVolumeRedX.6dc305f4.png"
+  },
+  {
+    "revision": "f45298583dd4c5e54010a228c673a321",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/DvdPowerOff.f4529858.png"
+  },
+  {
+    "revision": "21df8a04ee39b9f4b0a92081d8dab84d",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/DvdPowerOn.21df8a04.png"
+  },
+  {
+    "revision": "bce0855b5dde205878d2cb87e0c5c5ea",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/Eject.bce0855b.png"
+  },
+  {
+    "revision": "6b86399ce17640dfc9f983b5908bfa49",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/GreenDownGrey.6b86399c.png"
+  },
+  {
+    "revision": "a5cbd84ed5eda2bfe28ab1c74992d0ef",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/GreenDownKey.a5cbd84e.png"
+  },
+  {
+    "revision": "362f96ae2145af37df1f097159172d1d",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/GreenPowerOn.362f96ae.png"
+  },
+  {
+    "revision": "142b51e477371a3e5c0cbdd218f6d67e",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/GreenUpGrey.142b51e4.png"
+  },
+  {
+    "revision": "00b6bfcbd807da86abcf414fb90a60d0",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/GreenUpKey.00b6bfcb.png"
+  },
+  {
+    "revision": "518e7c578c992358574111e90bc2af09",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/LightRedPowerOff.518e7c57.png"
+  },
+  {
+    "revision": "ed2945c68db5c0581748961613773ed7",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/Pause.ed2945c6.png"
+  },
+  {
+    "revision": "fa077177de5f256bbe8ff9617a5264ac",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/Play.fa077177.png"
+  },
+  {
+    "revision": "78cbf16530e5e1047c8705d848023802",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/RedVolume.78cbf165.png"
+  },
+  {
+    "revision": "a5e883664ff0d7adea3f10cb3f667469",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/RedVolumeGrey.a5e88366.png"
+  },
+  {
+    "revision": "c32fd208b2b5f2d4972a580bc9b09062",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/Stop.c32fd208.png"
+  },
+  {
+    "revision": "f7ced528313719a5cf987bf988d8bd40",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/TVOffDvdOff.f7ced528.png"
+  },
+  {
+    "revision": "c6ea4ff697d17d311de8581fcf61c989",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/TVOffDvdOffDiskEjected.c6ea4ff6.png"
+  },
+  {
+    "revision": "b91910e741254c4d39d7b5ba76c3b030",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/TVOffDvdOn.b91910e7.png"
+  },
+  {
+    "revision": "d944d96393f260e31e901cdd984af6de",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/TVOffDvdOnDiskEjected.d944d963.png"
+  },
+  {
+    "revision": "24d9255153b547135eae566a066c0df5",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/TVOnDvdOff.24d92551.png"
+  },
+  {
+    "revision": "22625938ec6f852cc5a8f12a942b45ff",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/TVOnDvdOffDiskEjected.22625938.png"
+  },
+  {
+    "revision": "79f90726baed5e668677dd41530bcb31",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/TVOnDvdOn.79f90726.png"
+  },
+  {
+    "revision": "b747f8caef8e46022c19960a82625ae7",
+    "url": "/jfogelmanpyramid/TestScreenerApp/static/media/TVOnDvdOnDiskEjected.b747f8ca.png"
+  }
+]);
